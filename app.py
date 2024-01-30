3849426e462521883c547f262f09b7b3d2324682
Uddesh Takpere <92873831+uddesh18@users.noreply.github.com>
2024-01-30 15:51:04 +0530

    Update app.py
    mysql cred

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 bcrypt_new = Bcrypt(app) 
 
 # Configure MySQL
-app.config['MYSQL_HOST'] = ######
-app.config['MYSQL_USER'] = ######
-app.config['MYSQL_PASSWORD'] = ######
-app.config['MYSQL_DB'] = ######
+app.config['MYSQL_HOST'] = '##########'
+app.config['MYSQL_USER'] = '##########'
+app.config['MYSQL_PASSWORD'] = '##########'
+app.config['MYSQL_DB'] = '##########'
 
 mysql = MySQL(app)
 
